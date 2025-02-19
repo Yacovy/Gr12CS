f76ffb750cbf118e0cad508a950b78c04d93186f
Jacob <jacobgu00@gmail.com>
2025-02-19 14:48:25 -0500

    Lesson 8 complete

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,14 @@
 # Lesson 8
+wins = 0
+for i in range(6):
+    result = input(f"Input result for game {i+1} ")
+    if result == "W":
+        wins += 1
+if wins > 4:
+    print(1)
+elif wins > 2:
+    print(2)
+elif wins > 0:
+    print(3)
+else:
+    print(-1)
