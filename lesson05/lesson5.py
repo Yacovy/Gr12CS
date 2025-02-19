26260116635ce63b71548ab660d6f6cba99c9ab5
Jacob <jacobgu00@gmail.com>
2025-02-19 14:22:35 -0500

    Lesson 5 complete

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,8 @@
 # Lesson 5
+money = input("How much money did you start with ")
+cookies = input("What cookies did you sell ")
+bcookie = cookies.count("b")
+ncookie = cookies.count("c")
+print(f"you sold {len(cookies)} cookies")
+print(f"you made: {bcookie * 1.25 + ncookie * 0.75} dollars")
+print(f"Your total money now is: {int(money) + bcookie * 1.25 + ncookie * 0.75}")
