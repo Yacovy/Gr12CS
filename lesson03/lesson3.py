090df9b46dd41bf26df280be112fa10dd02b9ea9
Jacob <jacobgu00@gmail.com>
2025-02-19 14:17:41 -0500

    Lesson 3 completed

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,11 @@
-# Lesson 3
+inp = input("Enter the number of tiles: ")
+tiles = int(inp)
+count = 0
+check = 1
+
+
+count = tiles ** 0.5 // 1
+        
+print(f"The largest square has side length {tiles}.")
+
+
