f25cb210bffb9309cd273593c1c9bf9371fb6659
Jacob <jacobgu00@gmail.com>
2025-02-19 14:32:47 -0500

    Finished lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,9 @@
-# Lesson 7
+# Lesson
+import random
+DC = int(input("Please input the required DC "))
+D20 = random.randrange(0,21)
+print(D20)
+if D20 >= DC:
+    print("You passed the DC")
+else:
+    print("you failed the DC")
