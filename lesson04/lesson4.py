f1e1373234c0e1d87deef617606fdb48891e9b10
Jacob <jacobgu00@gmail.com>
2025-02-19 14:21:18 -0500

    Lesson 4 completed

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,11 @@
 # Lesson 4
+len1 = len(input())
+len2 = len(input())
+len3 = len(input())
+print(len1+len2+len3)
+print((len1+len2+len3)%12)
+
+if (len1+len2+len3) % 12 == 0:
+    print(((len1+len2+len3)/12)* 14.95)
+else:
+    print((((len1+len2+len3)//12)+1)* 14.95)
