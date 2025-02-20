403b253d7d8e49b4856733fe0e557c8f4d1d06b2
Jacob <jacobgu00@gmail.com>
2025-02-20 14:44:24 -0500

    Lesson finished

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,30 @@
 # Lesson 9
+
+import random
+invalid = True
+valid = ["scissors","rock","paper"]
+while invalid:
+    pick = input("Input rock, paper or scissors ")
+    if pick in valid:
+        invalid = False
+AI = random.choice(valid)
+if AI == pick:
+    print("The result is a tie")
+elif pick == "scissors":
+    if AI == "rock":
+        print("The result is a win for the AI")
+    if AI == "paper":
+        print("The result was a win for the Player")
+elif pick == "rock":
+    if AI == "paper":
+        print("The result is a win for the AI")
+    if AI == "scissors":
+        print("The result was a win for the Player")
+elif pick == "paper":
+    if AI == "rock":
+        print("The result was a win for the Player")
+    if AI == "scissors":
+         print("The result is a win for the AI")
+    
+
+    
