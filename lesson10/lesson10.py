325440f90e982b10cb3b934b068ef3591a27d489
Jacob <jacobgu00@gmail.com>
2025-02-20 14:42:13 -0500

    Lesson finished

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,14 @@
 # Lesson 10
+phoneNum = input("Input the last 4 digits of the phone num ")
+
+if not(phoneNum[0] == "8" or phoneNum[0] == "9"):
+    print("answer")
+
+elif not(phoneNum[3] == "8" or phoneNum[4] == "9"):
+    print("answer")
+elif not(phoneNum[2] == phoneNum[1]):
+    print("answer")
+else:
+    print("ignore")
+
+
