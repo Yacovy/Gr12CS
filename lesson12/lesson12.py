e736b26a4a4b9f8a00b9c6585219d2f2de2f826c
Jacob <jacobgu00@gmail.com>
2025-02-20 17:33:12 -0500

    lesson finished

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,14 @@
 # Lesson 12
+ang1 = int(input())
+ang2 = int(input())
+ang3 = int(input())
+total = ang1 + ang2 + ang3
+if total == 180:
+    if ang1 == ang2 == ang3:
+        print("Equilateral")
+    elif ang1 == ang2 or ang2 == ang3 or ang1 == ang3:
+        print("Isoceles")
+    else:
+        print("Scalene")
+else:
+    print("error")
