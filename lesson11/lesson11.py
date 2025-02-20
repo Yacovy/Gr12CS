555385ed4520b1509f461ef951e93b6f1489590e
Jacob <jacobgu00@gmail.com>
2025-02-20 17:13:25 -0500

    lesson finished

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,14 @@
 # Lesson 11
+inp = input()
+inp = inp.split(' ')
+inp = list(map(int,inp))
+x,y = inp
+if x > 0:
+    if y > 0: 
+        print('the point is in quadrant 1')
+    else:
+        print("the point is in quadrant 4")
+elif y>0:
+    print("the point is in quadrant 2 ")
+else:
+    print('the points is in quadrant 3')
