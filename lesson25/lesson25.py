4326265199c3770b38dcbd3b826422c90e5e036c
Jacob <jacobgu00@gmail.com>
2025-02-21 13:09:17 -0500

    lesson finished

--- lesson25/lesson25.py
+++ lesson25/lesson25.py
@@ -1,1 +1,15 @@
 # Lesson 25
+num = int(input())
+SubFactor = 0
+largest = 0
+for i in range(1,num):
+    SubFactor = 0
+    if num % i == 0:
+        for x in range(1,i):
+            if i % x == 0:
+                SubFactor += 1
+        if SubFactor == 1:
+            largest = i
+print(largest)
+
+
