21fed7865344e029595785701bbd5ee46f8afb48
Jacob <jacobgu00@gmail.com>
2025-02-21 11:44:40 -0500

    lesson finished

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,12 @@
 # Lesson 20
+sum = 0
+temp = 0
+for i in range(1,10001):
+    temp = 0
+    for x in range (1,i):
+        if i % x == 0:
+            temp += x
+    if temp == i:
+        sum += i
+print(sum)
+        
