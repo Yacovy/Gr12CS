c30e2e0ec4e33392a7b7487d92e02c63a01cf203
Jacob <jacobgu00@gmail.com>
2025-02-21 14:55:48 -0500

    lesson finished again

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,30 +1,33 @@
 # Lesson 9
 
 import random
-invalid = True
-valid = ["scissors","rock","paper"]
-while invalid:
-    pick = input("Input rock, paper or scissors ")
-    if pick in valid:
-        invalid = False
-AI = random.choice(valid)
-if AI == pick:
-    print("The result is a tie")
-elif pick == "scissors":
-    if AI == "rock":
-        print("The result is a win for the AI")
-    if AI == "paper":
-        print("The result was a win for the Player")
-elif pick == "rock":
-    if AI == "paper":
-        print("The result is a win for the AI")
-    if AI == "scissors":
-        print("The result was a win for the Player")
-elif pick == "paper":
-    if AI == "rock":
-        print("The result was a win for the Player")
-    if AI == "scissors":
-         print("The result is a win for the AI")
-    
+pick = ""
+while pick !="stop":
+    invalid = True
+    valid = ["scissors","rock","paper","stop"]
 
-    
+    while invalid:
+        pick = input("Input rock, paper or scissors or stop to stop ")
+        if pick in valid:
+            invalid = False
+    if pick !="stop":
+        AI = random.choice(valid)
+        if AI == pick:
+            print("The result is a tie")
+        elif pick == "scissors":
+            if AI == "rock":
+                print("The result is a win for the AI")
+            if AI == "paper":
+                print("The result was a win for the Player")
+        elif pick == "rock":
+            if AI == "paper":
+                print("The result is a win for the AI")
+            if AI == "scissors":
+                print("The result was a win for the Player")
+        elif pick == "paper":
+            if AI == "rock":
+                print("The result was a win for the Player")
+            if AI == "scissors":
+                print("The result is a win for the AI")
+
+
