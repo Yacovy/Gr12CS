23a5e80d4fc31bbc6f14900a1047cbc80db5a97e
Jacob <jacobgu00@gmail.com>
2025-02-21 11:16:39 -0500

    lesson finished

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,11 @@
 # Lesson 19
+inp = int(input())
+count = 0
+for i in range(2,inp):
+    if inp % i == 0:
+        count += 1
+        break
+if count != 0:
+    print("Not Prime")
+else:
+    print("Prime")
