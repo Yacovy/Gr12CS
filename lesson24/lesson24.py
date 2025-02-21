a768d13b5d14b0ecc1dcd7284d3b1c72bfde0f73
Jacob <jacobgu00@gmail.com>
2025-02-21 12:51:38 -0500

    lesson finished

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,12 @@
 # Lesson 24
+
+longest = 0 
+longestN = ""
+while 1==1:
+    name = input("input a name or input x to exit program ")
+    if name == "x":
+        break
+    if len(name) > longest:
+        longest = len(name)
+        longestN = name
+print(longestN)
