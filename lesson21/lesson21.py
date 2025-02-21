d69743e0222b2831927d83cc081b6d519300478c
Jacob <jacobgu00@gmail.com>
2025-02-21 11:43:45 -0500

    lesson finished

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,16 @@
 # Lesson 21
+max = int(input())
+count = 0
+temp = 0
+most = 0
+for i in range(1,max+1):
+    temp = 0
+    for x in range(1,i+1):
+        if i % x == 0:
+            temp += 1
+    if temp > count:
+        most = i
+        count = temp
+print(most)
+
+
