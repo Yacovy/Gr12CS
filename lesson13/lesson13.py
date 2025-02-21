81c12a533e00a8799591d500e4cf0cd1014579fc
Jacob <jacobgu00@gmail.com>
2025-02-21 10:46:23 -0500

    lesson finished

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,16 @@
 # Lesson 13
+month = int(input())
+day = int(input())
+
+if month > 2:
+    print("after")
+elif month < 2:
+    print("before")
+else:
+    if day > 18:
+        print("after")
+    elif day == 18:
+        print('during')
+    else: 
+        print('before')
+
