4db52b3cbc327732916925abd9d1b3168bddda40
Jacob <jacobgu00@gmail.com>
2025-02-21 12:42:01 -0500

    lesson finished

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,10 @@
 # Lesson 23
+sum = 0 
+counter = 0
+while 1 == 1:
+    inp = int(input("input a number or input -1 to exit the loop "))
+    if inp == -1:
+        break
+    sum += inp
+    counter += 1
+print (sum/counter)
